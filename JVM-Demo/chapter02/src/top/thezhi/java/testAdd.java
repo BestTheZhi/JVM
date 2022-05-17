f3bac2b5ad8e33d70103393a920bf44783578b0a
ZhiLiu <498324969@qq.com>
2022-05-17 22:10:05 +0800

    ba1a78b3-760d-4a4d-a4f9-fdc8c05d5be2

---
+++ JVM-Demo/chapter02/src/top/thezhi/java/testAdd.java
@@ -0,0 +1,21 @@
+package top.thezhi.java;
+
+/**
+ * @author ZHI LIU
+ * @date 2022-04-20
+ */
+
+
+public class testAdd {
+
+    public static void main(String[] args) {
+        System.out.println("javaEE");
+
+        System.out.println("hadoop");
+
+        System.out.println("javaEE" + "hadoop");
+
+
+    }
+
+}
